cc117d4a6e7e65c7e6e891e4ecf50303416a9435
vanessa082 <wahvanessa22@gmail.com>
2025-01-18 21:20:48 +0100

    resolved conversation

--- web/src/core/components/atoms/tables/analytics-table.tsx
+++ web/src/core/components/atoms/tables/analytics-table.tsx
@@ -3,30 +3,22 @@ export default function AnalyticsTable() {
     <table className="min-w-full bg-app-dark-400" role="table">
       <thead className="bg-app-dark-300 text-app-white-500">
         <tr>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-          >
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
             Short Link
           </th>
-          <th
-            scope="col"
-            className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider"
-          >
+          <th scope="col" className="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider">
             Original Link
           </th>
         </tr>
       </thead>
       <tbody>
         <tr>
-          <td
-            colSpan={2}
-            className="px-5 py-4 text-center text-sm italic text-app-white-500 bg-app-dark-200"
-          >
-            No history data detected You haven't shortened any links
+          <td colSpan={2} className="px-5 py-4 text-center text-sm italic text-app-white-500 bg-app-dark-200">
+            No history data detected
+            You haven &apos;t shortened any links
           </td>
         </tr>
       </tbody>
     </table>
-  );
+  )
 }
