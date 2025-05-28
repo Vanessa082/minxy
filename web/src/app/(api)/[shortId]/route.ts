56fd32899223bcd6881a952a43ce8be477ceb660
essa237 <wahvanessa22@gmail.com>
2025-05-28 18:00:09 +0100

    fixed build error

--- web/src/app/(api)/[shortId]/route.ts
+++ web/src/app/(api)/[shortId]/route.ts
@@ -1,17 +1,17 @@
 // import { redirect } from "next/navigation";
 
-import { connectDB } from "@/server/config/database";
+// import { connectDB } from "@/server/config/database";
 import { NextRequest } from "next/server";
 
 const minxyGoDaddy =
   "https://www.godaddy.com/en-ie/domainsearch/find?domainToCheck=minxy.dev&tmskey=sem_godaddy_brand";
 
 export async function GET(req: NextRequest) {
-  await connectDB();
+  // await connectDB();
 
   console.log("\nrequest\n", req.url, "\n");
 
-  // return Response.redirect(minxyGoDaddy);
+  return Response.redirect(minxyGoDaddy);
 
   // get url from params.shortId, fetch from database and redirect to the original url
   // return Response.json({
